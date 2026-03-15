8db1529839509118c2dc4643d09e63f0c2518046
Ali Hassan <hassanaliabbasi478@gmail.com>
2026-03-15 20:33:01 +0500

    update cros

--- agriback/settings.py
+++ agriback/settings.py
@@ -138,7 +138,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ALLOWED_ORIGINS = [
-    "https://agricare-production-547c.up.railway.app/api/",
-]
+CORS_ALLOWED_ORIGINS  = True
+CORS_ALLOWED_CREDENTIALS = True
