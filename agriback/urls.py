245779df13953b16b46c7dd2922f5d3da813e48f
Ali Hassan <hassanaliabbasi478@gmail.com>
2026-03-15 11:43:29 +0500

    Addinh home route

--- agriback/urls.py
+++ agriback/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("AgriCare Backend Running")
 urlpatterns = [
-
+path('', home),
 path('admin/',admin.site.urls),
 
 path('api/', include('api.urls')),
